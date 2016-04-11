c730f38242aff96df7392b4a0b89e51623ae6449
Dragon <manuelpetron@gmail.com>
2016-04-11 21:03:01 +0200

    Listo el xml y boton wifi con código funcionando y refrescando mejorado con publicidad

--- app/src/main/java/company/petron/wifiinfo/MainActivity_wifi_info.java
+++ app/src/main/java/company/petron/wifiinfo/MainActivity_wifi_info.java
@@ -1,37 +1,48 @@
 package company.petron.wifiinfo;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.startapp.android.publish.StartAppAd;
+import com.startapp.android.publish.StartAppSDK;
 
 public class MainActivity_wifi_info extends AppCompatActivity {
 
     TextView textEstado, textIP, textSSID, textSIDOculto;
-    TextView textBSSID, textMAC, textVelocidad, textRSSI;
+    TextView textBSSID, textMAC, textVelocidad, textRSSI,textSeñal;
     String infoIP, infoSSID, infoBSSID,
             infoSSIDOculto, infoEstado, infoRSSID,
             infoMAC, infoVelocidad,infoNodatos;
     WifiManager wifiManager;
+    int numseñal,nunrssi ;
+    String letseñal;
+    private StartAppAd startAppAd = new StartAppAd(this);
 
 
 
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StartAppSDK.init(this, "101423750", "203550305", true);
         setContentView(R.layout.activity_main_activity_wifi_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,88 +56,116 @@ public class MainActivity_wifi_info extends AppCompatActivity {
             }
         });*/
         wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
-        textEstado = (TextView)findViewById(R.id.txtEstado);
-        textIP = (TextView)findViewById(R.id.txtIP);
+        textEstado = (TextView) findViewById(R.id.txtEstado);
+        textIP = (TextView) findViewById(R.id.txtIP);
+        textSSID = (TextView) findViewById(R.id.txtSSID);
+        textSIDOculto = (TextView) findViewById(R.id.txtSSIDOculto);
+        textBSSID = (TextView) findViewById(R.id.txtBSSID);
+        textMAC = (TextView) findViewById(R.id.txtMAC);
+        textVelocidad = (TextView) findViewById(R.id.txtVelocidad);
+        textRSSI = (TextView) findViewById(R.id.txtRSSI);
+        textSeñal = (TextView) findViewById(R.id.txtseñal);
 
-        textSSID = (TextView)findViewById(R.id.txtSSID);
-        textSIDOculto = (TextView)findViewById(R.id.txtSSIDOculto);
-        textBSSID = (TextView)findViewById(R.id.txtBSSID);
-        textMAC = (TextView)findViewById(R.id.txtMAC);
-        textVelocidad = (TextView)findViewById(R.id.txtVelocidad);
-        textRSSI = (TextView)findViewById(R.id.txtRSSI);
+
 
         ConnectivityManager mAdministradorConexion =
                 (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo mInformacionRed =
                 mAdministradorConexion.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         WifiManager mAdministradorWifi =
-                (WifiManager)getSystemService(Context.WIFI_SERVICE);
+                (WifiManager) getSystemService(Context.WIFI_SERVICE);
         WifiInfo mInformacionWifi = mAdministradorWifi.getConnectionInfo();
-        if (mInformacionRed.isConnected())
-        {
-            int myIp = mInformacionWifi.getIpAddress();
-            int intMyIp3 = myIp/0x1000000;
-            int intMyIp3mod = myIp%0x1000000;
-            int intMyIp2 = intMyIp3mod/0x10000;
-            int intMyIp2mod = intMyIp3mod%0x10000;
-            int intMyIp1 = intMyIp2mod/0x100;
-            int intMyIp0 = intMyIp2mod%0x100;
 
-            infoIP = " "+ String.valueOf(intMyIp0)
+
+        if (mInformacionRed.isConnected()) {
+            int myIp = mInformacionWifi.getIpAddress();
+            int intMyIp3 = myIp / 0x1000000;
+            int intMyIp3mod = myIp % 0x1000000;
+            int intMyIp2 = intMyIp3mod / 0x10000;
+            int intMyIp2mod = intMyIp3mod % 0x10000;
+            int intMyIp1 = intMyIp2mod / 0x100;
+            int intMyIp0 = intMyIp2mod % 0x100;
+
+            infoIP = " " + String.valueOf(intMyIp0)
                     + "." + String.valueOf(intMyIp1)
                     + "." + String.valueOf(intMyIp2)
                     + "." + String.valueOf(intMyIp3);
             infoEstado = (getResources().getString(R.string.estado_conectado));
-            infoVelocidad = " "+ String.valueOf(
+            infoVelocidad = " " + String.valueOf(
                     mInformacionWifi.getLinkSpeed()) + " " +
                     WifiInfo.LINK_SPEED_UNITS;
-            infoSSID =  " "+ mInformacionWifi.getSSID();
-            infoBSSID = " "+ mInformacionWifi.getBSSID();
-            infoRSSID = (getResources().getString(R.string.rssi ))+ " " + mInformacionWifi.getRssi();
-            infoMAC = " " +  mInformacionWifi.getMacAddress();
+            infoSSID = " " + mInformacionWifi.getSSID();
+            infoBSSID = " " + mInformacionWifi.getBSSID();
+            infoRSSID = (getResources().getString(R.string.rssi)) + " " + mInformacionWifi.getRssi();
+            infoMAC = " " + mInformacionWifi.getMacAddress();
             if (!mInformacionWifi.getHiddenSSID())
                 infoSSIDOculto = (getResources().getString(R.string.ssid_oculto_no));
             else
                 infoSSIDOculto = (getResources().getString(R.string.ssid_oculto_si));
 
-            textIP.setText (getResources().getString(R.string.ip)+infoIP);
+            textIP.setText(getResources().getString(R.string.ip) + infoIP);
             textEstado.setText(infoEstado);
-            textVelocidad.setText(getResources().getString(R.string.velocidad)+ infoVelocidad);
-            textSSID.setText(getResources().getString(R.string.ssid)+ infoSSID);
+            textVelocidad.setText(getResources().getString(R.string.velocidad) + infoVelocidad);
+            textSSID.setText(getResources().getString(R.string.ssid) + infoSSID);
             textSIDOculto.setText(infoSSIDOculto);
-            textBSSID.setText (getResources().getString(R.string.bssid )+infoBSSID);
+            textBSSID.setText(getResources().getString(R.string.bssid) + infoBSSID);
             textRSSI.setText(infoRSSID);
-            textMAC.setText(getResources().getString(R.string.mac)+infoMAC);
-        }
-        else
-        {
+            textMAC.setText(getResources().getString(R.string.mac) + infoMAC);
+            nunrssi = (mInformacionWifi.getRssi());
+            numseñal = 100 + (mInformacionWifi.getRssi());
+            letseñal = String.valueOf(numseñal);
+            textSeñal.setText(getResources().getString(R.string.señal) +" " + letseñal + "%");
+
+
+        } else {
 
             infoEstado = (getResources().getString(R.string.estado_desconectado));
-            infoNodatos= " --";
+            infoNodatos = " --";
 
-            textEstado.setText (infoEstado);
-            textIP.setText(getResources().getString(R.string.ip)+infoNodatos);
-            textVelocidad.setText (getResources().getString(R.string.velocidad)+ infoNodatos);
-            textSSID.setText(getResources().getString(R.string.ssid)+ infoNodatos);
-            textSIDOculto.setText(getResources().getString(R.string.ssid_oculto)+ infoNodatos);
-            textBSSID.setText(getResources().getString(R.string.bssid)+ infoNodatos);
-            textRSSI.setText(getResources().getString(R.string.rssi)+ infoNodatos);
-            textMAC.setText (getResources().getString(R.string.mac)+ infoNodatos);
+            textEstado.setText(infoEstado);
+            textIP.setText(getResources().getString(R.string.ip) + infoNodatos);
+            textVelocidad.setText(getResources().getString(R.string.velocidad) + infoNodatos);
+            textSSID.setText(getResources().getString(R.string.ssid) + infoNodatos);
+            textSIDOculto.setText(getResources().getString(R.string.ssid_oculto) + infoNodatos);
+            textBSSID.setText(getResources().getString(R.string.bssid) + infoNodatos);
+            textRSSI.setText(getResources().getString(R.string.rssi) + infoNodatos);
+            textMAC.setText(getResources().getString(R.string.mac) + infoNodatos);
+            numseñal=0;
+            letseñal = String.valueOf(numseñal);
+            textSeñal.setText(getResources().getString(R.string.señal)+ " " + letseñal + "%");
         }
 
-        if (wifiManager.isWifiEnabled()){
-            mensajewifion();}
-        else {
+        if (wifiManager.isWifiEnabled()) {
+            mensajewifion();
+        } else {
             mensajewifioff();
         }
-
-
     }
 
 
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
 
 
 
+        //poner en marcha la publicidad al salir
+        MainActivity_wifi_info.this.startAppAd.showAd();
+        MainActivity_wifi_info.this.startAppAd.loadAd();
+
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        startAppAd.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        startAppAd.onPause();
+    }
 
 
     @Override
@@ -218,6 +257,27 @@ public class MainActivity_wifi_info extends AppCompatActivity {
             case R.id.ayuda:
                 //boton ayuda
 
+               /* Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                getResources().getString(R.string.toast_ayuda), Toast.LENGTH_SHORT);
+
+                toast1.show();*/
+
+                AlertDialog.Builder builder = new AlertDialog.Builder((Context)(this));
+                builder.setMessage((CharSequence)(getResources().getString(R.string.toast_ayuda)))
+                        .setIcon(R.mipmap.ic_launcher).setTitle((CharSequence) (getResources().getString(R.string.ayuda)))
+                        .setCancelable(false).setNeutralButton((CharSequence) (getResources().getString(R.string.ok)),
+                        (DialogInterface.OnClickListener) (new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialogInterface, int n) {
+                        dialogInterface.cancel();
+                    }
+                }));
+                builder.create().show();
+
+
+
+
                 return true;
 
             case R.id.votar:
@@ -228,6 +288,9 @@ public class MainActivity_wifi_info extends AppCompatActivity {
                 return true;
             case R.id.reiniciar:
 
+                MainActivity_wifi_info.this.startAppAd.showAd();
+                MainActivity_wifi_info.this.startAppAd.loadAd();
+
                 reinicio();
 
                 mensajeactualizar();
@@ -235,16 +298,11 @@ public class MainActivity_wifi_info extends AppCompatActivity {
                 return true;
             case R.id.salir:
                 //botón salir
-               // MainActivity_imei.this.startAppAd.showAd();
-               // MainActivity_imei.this.startAppAd.loadAd();
+                MainActivity_wifi_info.this.startAppAd.showAd();
+                MainActivity_wifi_info.this.startAppAd.loadAd();
                 finish();
                 return true;
-            case R.id.action_settings:
-                //botón salir
-                //MainActivity_imei.this.startAppAd.showAd();
-                //MainActivity_imei.this.startAppAd.loadAd();
-                finish();
-                return true;
+
 
 
             default:
