6b658da641b27bf095e807a7864f8c27c7d96b75
Manuel <androidtorrejoncillo1@gmail.com>
2017-08-29 10:35:40 +0200

    Vuesta a la 3.5.1 fallos en 3.6.5

--- app/src/main/java/company/petron/wifiinfo/MainActivity_wifi_info.java
+++ app/src/main/java/company/petron/wifiinfo/MainActivity_wifi_info.java
@@ -12,16 +12,15 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.startapp.android.publish.adsCommon.StartAppAd;
-import com.startapp.android.publish.adsCommon.StartAppSDK;
+
+import com.startapp.android.publish.StartAppAd;
+import com.startapp.android.publish.StartAppSDK;
 
 
 
@@ -50,7 +49,7 @@ public class MainActivity_wifi_info extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         textEstado = (TextView) findViewById(R.id.txtEstado);
         textIP = (TextView) findViewById(R.id.txtIP);
         textSSID = (TextView) findViewById(R.id.txtSSID);
@@ -68,7 +67,7 @@ public class MainActivity_wifi_info extends AppCompatActivity {
         NetworkInfo mInformacionRed =
                 mAdministradorConexion.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         WifiManager mAdministradorWifi =
-                (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         WifiInfo mInformacionWifi = mAdministradorWifi.getConnectionInfo();
 
 
